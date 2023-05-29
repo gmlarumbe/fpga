c8f36e8807aaaa16d2f063ef08d276c2d07b09fa
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2023-05-29 17:59:11 +0200

    Update synopsys and cadence utils

--- fpga-cadence.el
+++ fpga-cadence.el
@@ -23,30 +23,132 @@
 ;;; Commentary:
 
 ;; FPGA Cadence Utilities
-
+;;  - Compilation mode with syntax highlighting for Xcelium
+;;  - vManager VSIF mode
+;;
 ;;; Code:
 
 
 (require 'fpga-utils)
 
-;; Leveraged from verilog-mode (verilog-IES) and extended for UVM/OVM
-;;   - xrun-error: errors with line number and column number
-;;   - xrun-error2: errors with line number
-;;   - xrun-error3: errors without file/line
-(defvar larumbe/compilation-error-re-xrun
-  '((xrun-fatal    "\\(?1:^[a-z]+\\): \\(?2:\\*F\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)|\\(?6:[0-9]+\\)\\)" 4 5 6 2 nil (1 larumbe/compilation-binary-face) (2 compilation-error-face) (3 larumbe/compilation-gray-face))
-    (xrun-fatal2   "\\(?1:^[a-z]+\\): \\(?2:\\*F\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)\\)" 4 5 nil 2 nil (1 larumbe/compilation-binary-face) (2 compilation-error-face) (3 larumbe/compilation-gray-face))
-    (xrun-fatal3   "\\(?1:^[a-z]+\\): \\(?2:\\*F\\),\\(?3:[0-9A-Z]+\\):" 1 nil nil 2 nil (1 larumbe/compilation-binary-face) (2 compilation-error-face) (3 larumbe/compilation-gray-face))
-    (xrun-error    "\\(?1:^[a-z]+\\): \\(?2:\\*E\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)|\\(?6:[0-9]+\\)\\)" 4 5 6 2 nil (1 larumbe/compilation-binary-face) (2 compilation-error-face) (3 larumbe/compilation-gray-face))
-    (xrun-error2   "\\(?1:^[a-z]+\\): \\(?2:\\*E\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)\\)" 4 5 nil 2 nil (1 larumbe/compilation-binary-face) (2 compilation-error-face) (3 larumbe/compilation-gray-face))
-    (xrun-error3   "\\(?1:^[a-z]+\\): \\(?2:\\*E\\),\\(?3:[0-9A-Z]+\\):" 1 nil nil 2 nil (1 larumbe/compilation-binary-face) (2 compilation-error-face) (3 larumbe/compilation-gray-face))
-    (xrun-warning  "\\(?1:^[a-z]+\\): \\(?2:\\*W\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)|\\(?6:[0-9]+\\)\\)" 4 5 6 1 nil (1 larumbe/compilation-binary-face) (2 compilation-warning-face) (3 larumbe/compilation-gray-face))
-    (xrun-warning2 "\\(?1:^[a-z]+\\): \\(?2:\\*W\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)\\)" 4 5 nil 1 nil (1 larumbe/compilation-binary-face) (2 compilation-warning-face) (3 larumbe/compilation-gray-face))
-    (xrun-warning3 "\\(?1:^[a-z]+\\): \\(?2:\\*W\\),\\(?3:[0-9A-Z]+\\):" 1 nil nil 1 nil (1 larumbe/compilation-binary-face) (2 compilation-warning-face) (3 larumbe/compilation-gray-face))
-    (xrun-note     "\\(?1:^[a-z]+\\): \\(?2:\\*N\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)|\\(?6:[0-9]+\\)\\)" 4 5 6 0 nil (1 larumbe/compilation-binary-face) (2 compilation-info-face) (3 larumbe/compilation-gray-face))
-    (xrun-note2    "\\(?1:^[a-z]+\\): \\(?2:\\*N\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)\\)" 4 5 nil 0 nil (1 larumbe/compilation-binary-face) (2 compilation-info-face) (3 larumbe/compilation-gray-face))
-    (xrun-note3    "\\(?1:^[a-z]+\\): \\(?2:\\*N\\),\\(?3:[0-9A-Z]+\\):" 1 nil nil 0 nil (1 larumbe/compilation-binary-face) (2 compilation-info-face) (3 larumbe/compilation-gray-face))))
 
+;;;; Custom
+(defgroup fpga-cadence nil
+  "FPGA Cadence customization."
+  :group 'fpga)
+
+(defcustom fpga-cadence-xrun-buf "*xrun*"
+  "Buffer to use for Xcelium compilation process."
+  :type 'string
+  :group 'fpga-cadence)
+
+
+;;;; Compilation
+(defvar fpga-cadence-xrun-compile-re
+  '((xrun-fatal    "\\(?1:^[a-z]+\\): \\(?2:\\*F\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)|\\(?6:[0-9]+\\)\\)" 4 5 6 2 nil (1 fpga-utils-compilation-bin-face) (2 compilation-error-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-fatal2   "\\(?1:^[a-z]+\\): \\(?2:\\*F\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)\\)" 4 5 nil 2 nil (1 fpga-utils-compilation-bin-face) (2 compilation-error-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-fatal3   "\\(?1:^[a-z]+\\): \\(?2:\\*F\\),\\(?3:[0-9A-Z]+\\):" 1 nil nil 2 nil (1 fpga-utils-compilation-bin-face) (2 compilation-error-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-error    "\\(?1:^[a-z]+\\): \\(?2:\\*E\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)|\\(?6:[0-9]+\\)\\)" 4 5 6 2 nil (1 fpga-utils-compilation-bin-face) (2 compilation-error-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-error2   "\\(?1:^[a-z]+\\): \\(?2:\\*E\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)\\)" 4 5 nil 2 nil (1 fpga-utils-compilation-bin-face) (2 compilation-error-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-error3   "\\(?1:^[a-z]+\\): \\(?2:\\*E\\),\\(?3:[0-9A-Z]+\\):" 1 nil nil 2 nil (1 fpga-utils-compilation-bin-face) (2 compilation-error-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-warning  "\\(?1:^[a-z]+\\): \\(?2:\\*W\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)|\\(?6:[0-9]+\\)\\)" 4 5 6 1 nil (1 fpga-utils-compilation-bin-face) (2 compilation-warning-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-warning2 "\\(?1:^[a-z]+\\): \\(?2:\\*W\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)\\)" 4 5 nil 1 nil (1 fpga-utils-compilation-bin-face) (2 compilation-warning-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-warning3 "\\(?1:^[a-z]+\\): \\(?2:\\*W\\),\\(?3:[0-9A-Z]+\\):" 1 nil nil 1 nil (1 fpga-utils-compilation-bin-face) (2 compilation-warning-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-note     "\\(?1:^[a-z]+\\): \\(?2:\\*N\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)|\\(?6:[0-9]+\\)\\)" 4 5 6 0 nil (1 fpga-utils-compilation-bin-face) (2 compilation-info-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-note2    "\\(?1:^[a-z]+\\): \\(?2:\\*N\\),\\(?3:[0-9A-Z]+\\)\\(?:\\(?:\\[[0-9A-Z_,]+\\]\\)? (\\(?4:[^ \t,]+\\),\\(?5:[0-9]+\\)\\)" 4 5 nil 0 nil (1 fpga-utils-compilation-bin-face) (2 compilation-info-face) (3 fpga-utils-compilation-msg-code-face))
+    (xrun-note3    "\\(?1:^[a-z]+\\): \\(?2:\\*N\\),\\(?3:[0-9A-Z]+\\):" 1 nil nil 0 nil (1 fpga-utils-compilation-bin-face) (2 compilation-info-face) (3 fpga-utils-compilation-msg-code-face)))
+  "Xcelium compilation regexp.
+- xrun-error: errors with line number and column number
+- xrun-error2: errors with line number
+- xrun-error3: errors without file/line")
+
+(fpga-utils-define-compilation-mode fpga-cadence-xrun-compilation-mode
+  :desc "Xcelium"
+  :docstring "Xcelium Compilation mode."
+  :compile-re fpga-cadence-xrun-compile-re
+  :buf-name fpga-cadence-xrun-buf)
+
+(fpga-utils-define-compile-fn fpga-cadence-xrun-compile
+  :docstring "Compile Xcelium COMMAND with error regexp highlighting."
+  :buf fpga-cadence-xrun-buf
+  :comp-mode fpga-cadence-xrun-compilation-mode)
+
+
+;;;; vManager VSIF mode
+(defconst fpga-cadence-vsif-ntf-containers '("session" "group" "test" "extend"))
+(defconst fpga-cadence-vsif-session-container-attributes
+  '("abort_dependent_jobs_on_nonzero_exit" "abort_dependent_runs_on_failure"
+    "automation_file" "auto_load_vplan_refinement_vmgr" "auto_load_refinement_vmgr"
+    "auto_load_vplan_vmgr" "create_debug_logs" "create_optimized_coverage_data"
+    "default_dispatch_parameters" "description" "drm" "enable_session_recovery_vmgr"
+    "file_policy" "free_hosts" "group_automation_file" "host_lock_timeout"
+    "incremental_merge_type_vmgr" "incremental_merge_slice_size_vmgr"
+    "incremental_merge_timeout_to_merge_vmgr" "incremental_merge_runs_merged_vmgr"
+    "incremental_merge_last_time_merged_vmgr" "use_incremental_merged_model_for_analysis_vmgr"
+    "propose_failure_clusters_vmgr" "master_submission_policy" "local_top_dir"
+    "max_runs_in_parallel" "MERGE_CONFIG_VMGR" "model_dir" "output_mode"
+    "post_session_dispatch_parameters" "pre_session_dispatch_parameters"
+    "post_session_drm" "pre_session_drm" "post_session_script" "pre_session_script"
+    "top_dir" "verification_scope"))
+(defconst vsif-group-container-attributes '("bundle_group" "repetitions"))
+(defconst vsif-group-test-containers-attributes
+  '("bundle_group" "code_coverage" "count" "copied_files"
+    "depends_on" "details" "drm_job_priority_vmgr" "dut_name"
+    "estimated_duration_vmgr" "exit_on" "fully_directed" "gui_mode"
+    "hdl_files" "ifv_assertions" "ifv_effort" "ifv_engine"
+    "ifv_halo" "linked_files" "pre_group_script" "pre_run_script"
+    "primary_run" "post_commands" "post_group_script" "post_run_script"
+    "post_simulate_script" "repetitions" "revision" "run_mode"
+    "run_script" "run_on_emulation_hardware_vmgr" "xejob_args_vmgr" "runs_dispatch_parameters"
+    "scan_script" "seed" "sim_args" "sv_seed"
+    "sve_name" "test_command" "timeout" "top_files"
+    "verbosity" "waveform" "write_metrics" "use_ida_record"
+    "ida_db_start_time" "ida_probe_options" "ida_recording_window"))
+(defconst fpga-cadence-vsif-queuing_policy-options '("long2short" "short2long" "round_robin" "vsif_order"))
+
+(defconst fpga-cadence-vsif-ntf-containers-re
+  (eval-when-compile (regexp-opt fpga-cadence-vsif-ntf-containers 'symbols)))
+(defconst fpga-cadence-vsif-session-container-attributes-re
+  (eval-when-compile (regexp-opt fpga-cadence-vsif-session-container-attributes 'symbols)))
+(defconst fpga-cadence-vsif-group-container-attributes-re
+  (eval-when-compile (regexp-opt vsif-group-container-attributes 'symbols)))
+(defconst fpga-cadence-vsif-group-test-containers-attributes-re
+  (eval-when-compile (regexp-opt vsif-group-test-containers-attributes 'symbols)))
+(defconst fpga-cadence-vsif-queuing_policy-options-re
+  (eval-when-compile (regexp-opt fpga-cadence-vsif-queuing_policy-options 'symbols)))
+
+(defconst fpga-cadence-vsif-ntf-containers-re-font-lock
+  (concat "\\s-*" fpga-cadence-vsif-ntf-containers-re "\\s-+\\_<\\([a-zA-Z0-9_-]+\\)\\_>\\s-*"))
+(defconst fpga-cadence-vsif-cpp-directives-simple-re-font-lock
+  (concat "\\s-*" "\\(#[a-zA-Z0-9_]+\\)")) ; Only one word (e.g. #endif)
+(defconst fpga-cadence-vsif-cpp-directives-complex-re-font-lock
+  (concat "\\s-*" "\\(#[a-zA-Z0-9_]+\\)\\s-+\\(.*\\)")) ; More than one word (e.g. #ifdef asdf)
+(defconst fpga-cadence-vsif-group-test-containers-attributes-re-font-lock
+  (concat "\\s-*" fpga-cadence-vsif-group-test-containers-attributes-re "\\s-*:\\s-*\\([a-zA-Z0-9_-]+\\)\\s-*;"))
+(defconst fpga-cadence-vsif-queuing_policy-options-re-font-lock
+  (concat "\\s-*\\(queuing_policy\\)\\s-*:\\s-*" fpga-cadence-vsif-queuing_policy-options-re "\\s-*;"))
+(defconst fpga-cadence-vsif-user-options-re-font-lock (concat "\\s-*\\([a-zA-Z0-9_]+\\)\\s-*:\\s-*"))
+(defconst fpga-cadence-vsif-punctuation-re-font-lock "[:;]")
+(defconst fpga-cadence-vsif-attribute-fake-comment-re-font-lock "\\(^.*\\)\\(--\\s-*.*\\)$") ; INFO: These comments do not belong to syntax table comments, only font-locking
+
+(defvar fpga-cadence-vsif-font-lock-defaults
+  `(((,fpga-cadence-vsif-ntf-containers-re-font-lock . ((1 font-lock-keyword-face) (2 font-lock-function-name-face)))
+     (,fpga-cadence-vsif-group-test-containers-attributes-re-font-lock . (1 font-lock-variable-name-face))
+     (,fpga-cadence-vsif-queuing_policy-options-re-font-lock . (1 font-lock-variable-name-face))
+     (,fpga-cadence-vsif-cpp-directives-simple-re-font-lock . (1 font-lock-preprocessor-face))
+     (,fpga-cadence-vsif-cpp-directives-complex-re-font-lock . ((1 font-lock-preprocessor-face) (2 font-lock-variable-name-face)))
+     (,fpga-cadence-vsif-user-options-re-font-lock . (1 font-lock-variable-name-face))
+     (,fpga-cadence-vsif-punctuation-re-font-lock . fpga-utils-punctuation-face)
+     (,fpga-utils-curly-braces-re . fpga-utils-curly-braces-face)
+     (,fpga-cadence-vsif-attribute-fake-comment-re-font-lock . (2 font-lock-comment-face)))))
+
+;;;###autoload
+(define-derived-mode fpga-cadence-vsif-mode c-mode "VSIF"
+  "A mode for VSIF vManager regression files."
+  (setq-local font-lock-defaults fpga-cadence-vsif-font-lock-defaults))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist (cons (purecopy "\\.vsif\\'") 'fpga-cadence-vsif-mode))
 
 
 (provide 'fpga-cadence)
--- fpga-synopsys.el
+++ fpga-synopsys.el
@@ -23,79 +23,98 @@
 ;;; Commentary:
 
 ;; FPGA/ASIC Synopsys Utilities
-
+;;  - Improved Synopsys shell with syntax highlighting
+;;  - Compilation mode with syntax highlighting for Synplify
+;;  - Compilation regexps for design compiler (for potential extensions)
+;;
 ;;; Code:
 
+(require 'fpga-utils)
 
-;;;; Compilation-re
-(defvar fpga-lattice-compile-re-synplify
-  '((synp-error     "^@\\(?1:E\\): \\(?2:[A-Z0-9]+\\) :\"\\(?3:[0-9a-zA-Z./_-]+\\)\":\\(?4:[0-9]+\\):\\(?5:[0-9]+\\):" 3 4 5 2 nil (1 compilation-error-face) (2 fpga-lattice-compile-gray-face))
-    (synp-error2    "^@\\(?1:E\\): \\(?2:[A-Z0-9]+\\) [:]?|" 1 nil nil 2 nil (2 fpga-lattice-compile-gray-face))
+
+;;;; Custom
+(defgroup fpga-synopsys nil
+  "FPGA Synopsys customization."
+  :group 'fpga)
+
+(defcustom fpga-synopsys-synplify-bin (executable-find "synplify_premier")
+  "Path to Synplify executable."
+  :type 'string
+  :group 'fpga-synopsys)
+
+(defcustom fpga-synopsys-synplify-cmd-opts '("-shell" "-licensetype synplifypremierdp" "-verbose_log")
+  "Synplify process options."
+  :type '(repeat string)
+  :group 'fpga-synopsys)
+
+(defcustom fpga-synopsys-synplify-buf "*synplify*"
+  "Buffer to use for Synplify compilation process."
+  :type 'string
+  :group 'fpga-synopsys)
+
+(defcustom fpga-synopsys-synplify-shell-buf "*synplify-shell*"
+  "Buffer to use for Synplify interactive shell process."
+  :type 'string
+  :group 'fpga-synopsys)
+
+
+;;;; Internal
+(defconst fpga-synopsys-synplify--base-cmd
+  (concat fpga-synopsys-synplify-bin " " (mapconcat #'identity fpga-synopsys-synplify-cmd-opts " ")))
+
+
+;;;; Compilation
+(defconst fpga-synopsys-synplify-compile-re
+  '((synp-error     "^@\\(?1:E\\): \\(?2:[A-Z0-9]+\\) :\"\\(?3:[0-9a-zA-Z./_-]+\\)\":\\(?4:[0-9]+\\):\\(?5:[0-9]+\\):" 3 4 5 2 nil (1 compilation-error-face) (2 fpga-synplify-compile-gray-face))
+    (synp-error2    "^@\\(?1:E\\): \\(?2:[A-Z0-9]+\\) [:]?|" 1 nil nil 2 nil (2 fpga-synplify-compile-gray-face))
     (synp-error3    "^@\\(?1:E\\):" 1 nil nil 2 nil)
-    (synp-warning   "^@\\(?1:W\\): \\(?2:[A-Z0-9]+\\) :\"\\(?3:[0-9a-zA-Z./_-]+\\)\":\\(?4:[0-9]+\\):\\(?5:[0-9]+\\):" 3 4 5 1 nil (1 compilation-warning-face) (2 fpga-lattice-compile-gray-face))
-    (synp-warning2  "^@\\(?1:W\\): \\(?2:[A-Z0-9]+\\) [:]?|" 1 nil nil 1 nil (2 fpga-lattice-compile-gray-face))
+    (synp-warning   "^@\\(?1:W\\): \\(?2:[A-Z0-9]+\\) :\"\\(?3:[0-9a-zA-Z./_-]+\\)\":\\(?4:[0-9]+\\):\\(?5:[0-9]+\\):" 3 4 5 1 nil (1 compilation-warning-face) (2 fpga-synplify-compile-gray-face))
+    (synp-warning2  "^@\\(?1:W\\): \\(?2:[A-Z0-9]+\\) [:]?|" 1 nil nil 1 nil (2 fpga-synplify-compile-gray-face))
     (synp-warning3  "^@\\(?1:W\\):" 1 nil nil 1 nil)
-    (synp-note      "^@\\(?1:N\\): \\(?2:[A-Z0-9]+\\) :\"\\(?3:[0-9a-zA-Z./_-]+\\)\":\\(?4:[0-9]+\\):\\(?5:[0-9]+\\):" 3 4 5 0 nil (1 compilation-info-face) (2 fpga-lattice-compile-gray-face))
-    (synp-note2     "^@\\(?1:N\\): \\(?2:[A-Z0-9]+\\) [:]?|" 1 nil nil 0 nil (2 fpga-lattice-compile-gray-face))
+    (synp-note      "^@\\(?1:N\\): \\(?2:[A-Z0-9]+\\) :\"\\(?3:[0-9a-zA-Z./_-]+\\)\":\\(?4:[0-9]+\\):\\(?5:[0-9]+\\):" 3 4 5 0 nil (1 compilation-info-face) (2 fpga-synplify-compile-gray-face))
+    (synp-note2     "^@\\(?1:N\\): \\(?2:[A-Z0-9]+\\) [:]?|" 1 nil nil 0 nil (2 fpga-synplify-compile-gray-face))
     ;; Did not find what those meant online, so set as warnings
-    (synp-alt-info  "^@\\(?1:A\\): \\(?2:[A-Z0-9]+\\) :\"\\(?3:[0-9a-zA-Z./_-]+\\)\":\\(?4:[0-9]+\\):\\(?5:[0-9]+\\):" 3 4 5 0 nil (1 compilation-info-face) (2 fpga-lattice-compile-gray-face))
-    (synp-alt-info2 "^@\\(?1:A\\): \\(?2:[A-Z0-9]+\\) [:]?|" 1 nil nil 0 nil (2 fpga-lattice-compile-gray-face))
+    (synp-alt-info  "^@\\(?1:A\\): \\(?2:[A-Z0-9]+\\) :\"\\(?3:[0-9a-zA-Z./_-]+\\)\":\\(?4:[0-9]+\\):\\(?5:[0-9]+\\):" 3 4 5 0 nil (1 compilation-info-face) (2 fpga-synplify-compile-gray-face))
+    (synp-alt-info2 "^@\\(?1:A\\): \\(?2:[A-Z0-9]+\\) [:]?|" 1 nil nil 0 nil (2 fpga-synplify-compile-gray-face))
     (synp-alt-info3 "^@\\(?1:A\\):" 1 nil nil 0 nil)
     (synp-note3     "^@\\(?1:N\\):" 1 nil nil 0 nil)
     (synp-info      "^@\\(?1:I\\):" nil nil nil 0 nil (1 compilation-line-face))
     (synp-log       "^@\\(?1:L\\):" nil nil nil 0 nil (1 compilation-line-face)))
   "Synopsys Synplify regexps.")
 
+(fpga-utils-define-compilation-mode fpga-synopsys-synplify-compilation-mode
+  :desc "Synplify"
+  :docstring "Synplify Compilation mode."
+  :compile-re fpga-synopsys-synplify-compile-re
+  :buf-name fpga-synopsys-synplify-buf)
 
-(defvar fpga-misc-compile-re-design-compiler
-  '((dc-error     "\\(?1:^Error\\):  \\(?2:[0-9a-zA-Z./_-]+\\):\\(?3:[0-9]+\\): "       2 3   nil 2 nil (1 compilation-error-face))
-    (dc-error-2   "\\(?1:^Error\\): .*"                                                 1 nil nil 2 nil)
-    (dc-warning   "\\(?1:^Warning\\):  \\(?2:[0-9a-zA-Z./_-]+\\):\\(?3:[0-9]+\\): "     2 3   nil 1 nil (1 compilation-warning-face))
-    (dc-warning-2 "\\(?1:^Warning\\): .*"                                               1 nil nil 1 nil)
-    (dc-info      "\\(?1:^Information\\):  \\(?2:[0-9a-zA-Z./_-]+\\):\\(?3:[0-9]+\\): " 2 3   nil 0 nil (1 compilation-info-face))
-    (dc-info-2    "\\(?1:^Information\\): .*"                                           1 nil nil 0 nil))
+(fpga-utils-define-compile-fn fpga-synopsys-synplify-compile
+  :docstring "Compile Synplify COMMAND with error regexp highlighting."
+  :buf fpga-synopsys-synplify-buf
+  :comp-mode fpga-synopsys-synplify-compilation-mode)
+
+
+;;;; Synplify Shell
+;;;###autoload (autoload 'fpga-synopsys-synplify-shell "fpga-synopsys.el")
+(fpga-utils-define-shell-mode fpga-synopsys-synplify-shell
+  :bin fpga-synopsys-synplify-bin
+  :base-cmd fpga-synopsys-synplify--base-cmd
+  :shell-commands nil ; No auto-completion/font-lock yet
+  :comile-re fpga-synopsys-synplify-compile-re
+  :buf fpga-synopsys-synplify-shell-buf
+  :font-lock-kwds nil) ; No extra font-lock yet
+
+
+;;;; Misc
+;; Design Compiler
+(defconst fpga-synopsys-design-compiler-compile-re
+  '((dc-error     "\\(?1:^Error\\):  \\(?2:[0-9a-zA-Z./_-]+\\):\\(?3:[0-9]+\\): " 2 3 nil 2 nil (1 compilation-error-face))
+    (dc-error-2   "\\(?1:^Error\\): .*" 1 nil nil 2 nil)
+    (dc-warning   "\\(?1:^Warning\\):  \\(?2:[0-9a-zA-Z./_-]+\\):\\(?3:[0-9]+\\): " 2 3 nil 1 nil (1 compilation-warning-face))
+    (dc-warning-2 "\\(?1:^Warning\\): .*" 1 nil nil 1 nil)
+    (dc-info      "\\(?1:^Information\\):  \\(?2:[0-9a-zA-Z./_-]+\\):\\(?3:[0-9]+\\): " 2 3 nil 0 nil (1 compilation-info-face))
+    (dc-info-2    "\\(?1:^Information\\): .*" 1 nil nil 0 nil))
   "Synopsys Design Compiler regexps.")
-
-
-;;;; Synplify-TCL Shell
-(defvar fpga-lattice-synplify-shell-bin (executable-find "synplify_premier"))
-(defvar fpga-lattice-synplify-shell-cmd-switches
-  '("-shell"
-    "-licensetype synplifypremierdp"
-    "-verbose_log"))
-(defvar fpga-lattice-synplify-shell-buffer "*synplify-tcl*")
-
-
-;;;###autoload
-(defun fpga-lattice-synplify-shell ()
-  "Invoke a TCL Synplify shell with the proper regexps, suited for compilation."
-  (interactive)
-  (unless fpga-lattice-synplify-shell-bin
-    (error "Could not find synplify in $PATH.  Add it or set `fpga-lattice-synplify-shell-bin'"))
-  (let ((command (concat fpga-lattice-synplify-shell-bin " " (mapconcat #'identity fpga-lattice-synplify-shell-cmd-switches " ")))
-        (bufname fpga-lattice-synplify-shell-buffer)
-        (parser  "synplify"))
-    (larumbe/compilation-interactive command bufname parser)
-    (fpga-lattice-synplify-shell-completion-at-point-mode 1)
-    (company-mode 1)))
-
-
-(defun fpga-lattice-synplify-shell-tcl-send-line-or-region-and-step ()
-  "Send the current line to the inferior shell and step to the next line.
-When the region is active, send the region instead."
-  (interactive)
-  (let (from to end (proc (get-buffer-process fpga-lattice-synplify-shell-buffer)))
-    (if (use-region-p)
-        (setq from (region-beginning)
-              to (region-end)
-              end to)
-      (setq from (line-beginning-position)
-            to (line-end-position)
-            end (1+ to)))
-    (comint-send-string proc (buffer-substring-no-properties from to))
-    (comint-send-string proc "\n")
-    (goto-char end)))
-
 
 
 (provide 'fpga-synopsys)
