c0b40fa6e70317ff83baa40f9e2c8572d1483090
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2023-09-13 14:02:13 +0200

    Remove unnecessary Version header section in non-main files

--- fpga-altera.el
+++ fpga-altera.el
@@ -4,7 +4,6 @@
 
 ;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
 ;; URL: https://github.com/gmlarumbe/fpga
-;; Version: 0.1.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
