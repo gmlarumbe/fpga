23969f10928a070a86869f5287df0ebae081e113
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2025-03-18 14:48:48 +0100

    Update copyright year

--- fpga-siemens.el
+++ fpga-siemens.el
@@ -1,6 +1,6 @@
 ;;; fpga-siemens.el --- FPGA & ASIC Siemens Utils  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022-2024 Gonzalo Larumbe
+;; Copyright (C) 2022-2025 Gonzalo Larumbe
 
 ;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
 ;; URL: https://github.com/gmlarumbe/fpga
