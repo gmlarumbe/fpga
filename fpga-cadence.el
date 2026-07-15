cbf972cf8700291d904f8b922f5d919295d69a67
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2026-07-15 19:13:09 +0200

    Update copyright year

--- fpga-cadence.el
+++ fpga-cadence.el
@@ -1,6 +1,6 @@
 ;;; fpga-cadence.el --- FPGA & ASIC Cadence Utils  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022-2025 Gonzalo Larumbe
+;; Copyright (C) 2022-2026 Gonzalo Larumbe
 
 ;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
 ;; URL: https://github.com/gmlarumbe/fpga
